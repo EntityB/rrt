4ecdc8516400b845830e4983004d5c9eae17a746
Entity Black <entityblack@gmail.com>
2016-08-26 23:18:20 +0200

    index fixes

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-// module.exports = require("./lib/eet.js");
-
-e = require("./lib/eet.js");
-
-e.eetTest(function() { console.log(arguments)});
-
-console.log(__dirname);
+module.exports = require("./lib/eet.js");
